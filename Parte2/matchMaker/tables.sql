f77df4b2c79e42ec382d5f459f5d830456d3498f
junin17 <carlosarju@gmail.com>
2016-06-19 21:19:06 -0300

    aaa

---
+++ Parte2/matchMaker/tables.sql
@@ -0,0 +1,45 @@
+﻿create table usuario(
+	id serial primary key,
+	login varchar(40) unique,
+	senha varchar(40),
+	nome varchar(20),
+	sobrenome varchar(20),
+	email varchar(20),
+	sexo varchar(1),
+	dataNascimento timestamp
+);
+
+create table mensagem(
+	id serial primary key,
+	conteudo varchar(1024),
+	remetente integer references usuario(id),
+	destinatario integer references usuario(id),
+	dataMensagem timestamp,
+	titulo varchar(80)
+);
+
+create table grupo(
+	id serial primary key,
+	nome varchar(20)
+);
+
+create table usuariogrupo(
+	idusuario integer references usuario(id),
+	idgrupo integer references grupo(id)
+);
+
+create table jogo(
+	id serial primary key,
+	nome varchar(20)
+);
+
+create table amizade(
+	idusuario integer references usuario(id),
+	amigode integer references usuario(id)
+);
+
+create table usuariojogo(
+	idusuario integer references usuario(id),
+	idjogo integer references jogo(id)
+);
+
