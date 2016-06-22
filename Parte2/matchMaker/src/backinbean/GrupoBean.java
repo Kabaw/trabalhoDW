b62016f2b79836795405b1656223fc5072d96aa2
junin17 <carlosarju@gmail.com>
2016-06-22 17:40:09 -0300

    aaaa

--- Parte2/matchMaker/src/backinbean/GrupoBean.java
+++ Parte2/matchMaker/src/backinbean/GrupoBean.java
@@ -10,13 +10,34 @@ import javax.faces.bean.SessionScoped;
 import org.primefaces.model.DualListModel;
 
 import dao.UsuarioDAO;
+import model.Grupo;
 import model.Usuario;
 
 @ManagedBean(name = "grupoBean")
 @SessionScoped
 public class GrupoBean {
+	private Grupo grupo;
 	private DualListModel<Usuario> usuarios;
 	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private String titulo;
+	
+	@PostConstruct
+	public void init() {
+		grupo = new Grupo();
+		List<Usuario> usuariosDB = usuarioDAO.listar();
+		List<Usuario> selecionados = new ArrayList<Usuario>();
+		usuarios = new DualListModel<Usuario>(usuariosDB, selecionados);
+		titulo = "Novo Grupo";
+		
+	}
+	
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
 
 	public DualListModel<Usuario> getUsuarios() {
 		return usuarios;
@@ -27,12 +48,15 @@ public class GrupoBean {
 		this.usuarios = usuarios;
 	}
 	
-	@PostConstruct
-	public void init() {
-		List<Usuario> usuariosDB = usuarioDAO.listar();
-		List<Usuario> selecionados = new ArrayList<Usuario>();
-		usuarios = new DualListModel<Usuario>(usuariosDB, selecionados);
-		
+	public String getTitulo() {
+		return titulo;
 	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	
 	
 }
